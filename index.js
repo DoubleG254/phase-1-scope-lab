e55382aab3d127a1fe9b9745d1924f7cf77d8c01
DoubleG254 <glorygweth6@gmail.com>
2023-06-30 08:43:12 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your solution in this file!
+var customerName = "bob"
+function upperCaseCustomerName(){ 
+   return customerName="BOB"
+}
+function setBestCustomer(){
+     bestCustomer= "not bob"
+}
+function overwriteBestCustomer(string){
+      bestCustomer=`${string}`
+}
+const leastFavoriteCustomer = "Andy"
+function changeLeastFavoriteCustomer(string){
+    leastFavoriteCustomer=`${string}`
+}
